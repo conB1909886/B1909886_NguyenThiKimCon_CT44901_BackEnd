80134bdee7dac518f8bb5b5e7327a01f52d73ca7
conb1909886 <conb1909886@student.ctu.edu.vn>
2023-02-16 15:44:30 +0700

    Cai dat xu ly loi

---
+++ app/api-error.js
@@ -0,0 +1,10 @@
+class ApiError extends Error {
+    constructor (statusCode, message) {
+        supper();
+        this.statusCode = statusCode;
+        this.message = message;
+
+    }
+}
+
+module.exports = ApiError;
