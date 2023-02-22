8671fcb307a1c12ebd34b48ebcb3e57411b33ddc
conb1909886 <conb1909886@student.ctu.edu.vn>
2023-02-22 16:27:37 +0700

    Cai dat cac handler truy xuat CSDL

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,7 +1,45 @@
-exports.create = (req, res) => {
-    res.send({ message: "create handler"});
+const ContactService = require("../services/contact.service");
+const MongoDB = require("../utils/mongodb.util");
+const ApiError = require("../api-error");
 
+exports.findAll = async(req, res, next) => {
+    let documents = [];
+    try {
+        const contactService = new ContactService(MongoDB.client);
+        const { name } = req.query;
+        if (name) {
+            documents = await contactService.findByName(name);
+        } else {
+            documents = await contactService.findAll({});
+        }
+    } catch (error) {
+        return next(
+            new ApiError(500, " An error occurred while retrieving contacts")
+        );
+    }
+    return res.send(documents);
 };
+
+exports.create = async (req, res, next) => {
+    if (!req.body?.name) {
+        return next(new ApiError(400, "name can not be empty"));
+
+    }
+    try {
+        const contactService = new ContactService(MongoDB.client);   
+        const document = await contactService.create(req.body);
+        return res.send(document);
+
+    } catch (error){
+        return next(
+            new ApiError(500, "An error occurred while creating the contact")
+
+        );
+
+    }
+};
+   
+
 
 exports.findALL = (req, res) => {
     res.send({ message: "findALL handler"});
